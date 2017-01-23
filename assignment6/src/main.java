cdf99eb919c81a65afddad19acfd90483732550d
riwdo <racso.hansson@gmail.com>
2017-01-23 12:02:04 +0100

    Completed assignment 6

--- assignment6/src/main.java
+++ assignment6/src/main.java
@@ -1,21 +1,24 @@
 /**
- * Created by Oscar on 2017-01-22.
+ * Created by Oscar on 2017-01-23.
  */
-public class main {
+
+    public class main {
 
 
-    public static int BinomialC(int n, int k){
-        if(k < 0 || n < k){
-            return 0;
-        }else if(k == 0 || n== k){
-            return 1;
-        }else {
-            return BinomialC(n-1,k-1) + BinomialC(n-1,k);
+        public static int BinomialC(int n, int k){
+            if(k < 0 || n < k){
+                return 0;
+            }else if(k == 0 || n== k){
+                return 1;
+            }else {
+                return BinomialC(n-1,k-1) + BinomialC(n-1,k);
+            }
         }
+
+        public static void main(String[] args){
+            System.out.println(BinomialC(4,1));
+        }
+
     }
 
-    public static void main(String[] args){
-        System.out.println(BinomialC(4,1));
-    }
 
-}
